9ac10d5fff85252e349782970486584ab45ca9c6
m--i <m--i@awiszus.info>
2014-12-06 02:29:26 +0100

    CrawlerMain nun nur für Eingaben zuständig, Controller steuert Crawler und StatusProcessor. Bedienung optimiert.

--- code/TwitterCrawler/src/main/Controller.java
+++ code/TwitterCrawler/src/main/Controller.java
@@ -1,7 +1,10 @@
 package main;
 
+import java.io.IOException;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import twitter4j.Status;
 
@@ -13,111 +16,140 @@ import twitter4j.Status;
  * @version 1.0
  * 
  */
-public class Controller implements Runnable {
-
-    // timeout for connection to twitter
-    // private static int TIMEOUT = 3600; // 10minutes
-
-    private StreamListener listener;
-    // private ConcurrentLinkedQueue<Status> queue;
-    // private Thread crawler;
-    private int timeout;
-    private Logger logger;
-    private StatusProcessor[] worker;
-    private Thread[] workerThreads;
-
-    /**
-     * initialize a controller instance that coordinates the crawler-thread with
-     * the worker-threads
-     * 
-     * @param crawler
-     *            the crawler-thread as Thread
-     * @param worker
-     *            the worker-threads as array of Threads
-     * @param listener
-     *            the listener object that gets executed by the crawler-thread
-     *            as StreamListener
-     * @param queue
-     *            the queue where the crawler enqueues all the status objects
-     *            for the worker threads as ConcurrentLinkedQueue<Status>
-     * @param logger
-     *            a global logger for the whole program as Logger
-     * @param timeout
-     *            the timeout in seconds (0 for infinity) as Integer
-     */
-    public Controller(Thread crawler, StatusProcessor[] worker,
-            Thread[] workerThreads, StreamListener listener,
-            ConcurrentLinkedQueue<Status> queue, Logger logger, int timeout) {
-        this.listener = listener;
-        // this.queue = queue;
-        this.workerThreads = workerThreads;
-        // this.crawler = crawler;
-        this.logger = logger;
-        this.worker = worker;
-        this.timeout = timeout;
+public class Controller extends Thread {
+	private final static int THREADNUM = 15;
+	
+	private ConcurrentLinkedQueue<Status> statusQueue;
+	private Logger log;
+	private String sqlPassword;
+	private int runtime;
+	private StreamListener streamListener;
+	private Thread thrdStreamListener;
+	private Thread[] thrdStatusProcessor = new Thread[THREADNUM];
+	private StatusProcessor[] statusProcessor = new StatusProcessor[THREADNUM];
+	
+	/**
+	 * 
+	 * @param timeout the timeout in seconds (0 for infinity) as Integer
+	 * @param pwd the password for the root user of the database twitter as String
+	 * @throws SecurityException
+	 * @throws IOException
+	 */
+    public Controller(int timeout, String pwd) throws SecurityException, IOException {
+    	log = getLogger();
+    	log.info("Controller started");
+    	sqlPassword  = pwd;
+    	runtime = timeout;
+    	
+    	statusQueue = new ConcurrentLinkedQueue<Status>();
     }
 
-    @Override
-    public void run() {
-       
-        int c = 0;
-        if (timeout == 0){
-            c = -1;
-        }
+	@Override
+	public void run() {
+		// create thread to pull status from twitter:
+		streamListener = new StreamListener(statusQueue, log, false);
+		thrdStreamListener = new Thread(streamListener);
+		thrdStreamListener.start();
+		log.info("Crawler started");
+		
+		// create threads that extract informations of status and store them in the db
+		for (int i = 0; i < THREADNUM; ++i) {
+			statusProcessor[i] = new StatusProcessor(statusQueue, log, sqlPassword);
+			thrdStatusProcessor[i] = new Thread(statusProcessor[i]);
+			thrdStatusProcessor[i].start();
+		}
+		log.info("StatusProcessors started");
+		
+		if (runtime >= 0) {
+			new java.util.Timer().schedule( 
+			        new java.util.TimerTask() {
+			            @Override
+			            public void run() {
+			                shutdown();
+			                System.exit(0);
+			            }
+			        }, 
+			        runtime*1000
+			);
+		}
 
-        while (c < timeout) {
+	}
 
-            // if (!crawler.isAlive()) {
-            // // restart crawler
-            // Thread crawler = new Thread(listener);
-            // this.crawler = crawler;
-            // crawler.start();
-            // }
-
-            // sleep for 1s
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                logger.warning(e.getMessage() + "\n");
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+	/**
+	 * Shut down server
+	 */
+	public void shutdown() {
+		boolean success = true;
+		System.out.print("\n Terminating crawler..");
+		streamListener.exit();
+		try {
+			thrdStreamListener.join();
+		} catch (InterruptedException e) {
+			System.out.print(". Error (" + e.getMessage() + ").");
+			log.warning(e.getMessage());
+			success = false;
+		}
+		if (success) {
+			System.out.println(". done.");
+		}
+		
+		success = true;
+		System.out.print(" Terminating status processors..");
+		while (getQueueSize() > 0) {
+			try {
+				wait(100);
+			} catch (InterruptedException e) {
+				System.out.print(". Error (" + e.getMessage() + ").");
+				log.warning(e.getMessage());
+				success = false;
+			}
+		}
+		if (success) {
+			for (int i = 0; i < THREADNUM; ++i) {
+				try {
+					statusProcessor[i].run = false;
+					thrdStatusProcessor[i].join();
+				} catch (InterruptedException e) {
+					System.out.print(". Error (" + e.getMessage() + ").");
+					log.warning(e.getMessage());
+					success = false;
+					break;
+				}
+			}
+			if (success) {
+				System.out.println(". done.");
+			}
+		}
+		log.info("Program terminated by user");
+	}
+	
+	private Logger getLogger() throws SecurityException, IOException {
+		Logger l = Logger.getLogger("logger");
+		FileHandler fh = new FileHandler("LogFile.log", true);
+		SimpleFormatter formatter = new SimpleFormatter();
+		fh.setFormatter(formatter);
+		l.addHandler(fh);
+		// true: print output on console and into file
+		// false: only store output in logFile
+		l.setUseParentHandlers(false);
+		return l;
+	}
+	
+	public int getQueueSize() {
+		return statusQueue.size();
+	}
+	
+	@Override
+	public String toString() {
+        int threadsAlive = 0;
+        for (int i = 0; i < thrdStatusProcessor.length; i++) {
+            if (thrdStatusProcessor[i].isAlive()) {
+            	threadsAlive++;
             }
-            
-            if (timeout != 0) {
-                c += 1;
-            }
         }
-
-        listener.exit();
-        logger.info("Crawler terminated by the Controller");
-
-        // worker stop if queue is empty
-        for (int i = 0; i < worker.length; i++) {
-            worker[i].run = false;
-        }
-
-        // while (!queue.isEmpty()) {
-        // try {
-        // Thread.sleep(50);
-        // } catch (InterruptedException e) {
-        // logger.warning(e.getMessage() + "\n");
-        // // TODO Auto-generated catch block
-        // e.printStackTrace();
-        // }
-        // }
-
-        // wait till all workers ended
-        for (int i = 0; i < workerThreads.length; i++) {
-            try {
-                workerThreads[i].join();
-            } catch (InterruptedException e) {
-                logger.warning(e.getMessage());
-                // TODO
-                e.printStackTrace();
-            }
-        }
-
-        logger.info("Program terminated");
-        System.exit(0);
-    }
+		return " STATE OF THE CRAWLER: " + "\n"
+				+ " Number of status-objects in queue: " + getQueueSize() + "\n"
+				+ " Status of the Streamlistener: " + "\n"
+				+ " Number of running workers: " + threadsAlive + "/" + THREADNUM;	
+	}
 }
--- code/TwitterCrawler/src/main/CrawlerMain.java
+++ code/TwitterCrawler/src/main/CrawlerMain.java
@@ -3,13 +3,6 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
-import twitter4j.Status;
 
 /**
  * main class to collect data from twitter over the twitter stream api with
@@ -20,195 +13,69 @@ import twitter4j.Status;
  * 
  */
 public class CrawlerMain {
-
-    /**
-     * number of worker-threads
-     */
-    private static int THREADNUM = 15;
-
-    /**
-     * starts a crawler, that collects data from twitter
-     * 
-     * @param args
-     *            1. Argument: The run-time in seconds that the crawler should
-     *            run (0 for infinity); 2. Argument: The password for the root
-     *            user of the database twitter; no more arguments are required
-     */
+	private static Controller cntrl;
+//    /**
+//     * number of worker-threads
+//     */
+//    private static int THREADNUM = 15;
+//
+//    /**
+//     * starts a crawler, that collects data from twitter
+//     * 
+//     * @param args
+//     *            1. Argument: The run-time in seconds that the crawler should
+//     *            run (0 for infinity); 2. Argument: The password for the root
+//     *            user of the database twitter; no more arguments are required
+//     */
     public static void main(String[] args) {
 
         // only numbers from 0-9
         if (args.length > 1 && args[0].matches("[0-9]+")
                 && args[1].length() > 0) {
-            coordinator(Integer.parseInt(args[0]), args[1]);
+        	try {
+				cntrl = new Controller(Integer.parseInt(args[0]), args[1]);
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	cntrl.start();
+        	userInput();
         } else {
-            System.out.println("Error: Wrong argument!");
-        }
-
-    }
-
-    /**
-     * starts required threads to collect data
-     * 
-     * @param time
-     *            the run-time in seconds that the crawler should run (0 for
-     *            infinity) as Integer
-     * @param pw
-     *            the password for the root user of the database twitter as
-     *            String
-     */
-    private static void coordinator(int time, String pw) {
-        ConcurrentLinkedQueue<Status> statusQueue = new ConcurrentLinkedQueue<Status>();
-
-        Logger log = null;
-        // get logger
-        try {
-            log = getLogger();
-        } catch (SecurityException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-
-        // create thread to pull status from twitter
-        StreamListener sl = new StreamListener(statusQueue, log, false);
-        Thread crawler = new Thread(sl);
-        crawler.start();
-        log.info("Crawler started");
-
-        // create threads that extract informations of status and store them in
-        // the db
-        Thread worker[] = new Thread[THREADNUM];
-        StatusProcessor[] workerObject = new StatusProcessor[THREADNUM];
-        for (int i = 0; i < THREADNUM; i++) {
-            workerObject[i] = new StatusProcessor(statusQueue, log, pw);
-            Thread thread = new Thread(workerObject[i]);
-            worker[i] = thread;
-            worker[i].start();
-        }
-        log.info("StatusProcessors started");
-
-        // controller to stop other threads after a specified time
-        Thread c = new Thread(new Controller(crawler, workerObject, worker, sl,
-                statusQueue, log, time));
-        c.start();
-        log.info("Controller started");
-
-        manuelExit(sl, log, worker, workerObject, statusQueue);
-
-        // join threads
-
-        try {
-            crawler.join();
-        } catch (InterruptedException e) {
-            log.warning(e.getMessage());
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        for (int i = 0; i < THREADNUM; i++) {
-            try {
-                worker[i].join();
-            } catch (InterruptedException e) {
-                log.warning(e.getMessage());
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-
-        try {
-            c.join();
-        } catch (InterruptedException e) {
-            log.warning(e.getMessage());
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            System.out.println(" Error. Wrong argument. Using: crawler [runtime] [password]");
+            System.out.println("  runtime:\t The run-time in seconds that the crawler should run.");
+            System.out.println("  password:\t The password for the root user of the database twitter.");
         }
     }
 
-    /**
-     * user can exit program via console
-     * 
-     * @param sl
-     * @param logger
-     * @param workerThreads
-     * @param worker
-     */
-    private static void manuelExit(StreamListener sl, Logger logger,
-            Thread[] workerThreads, StatusProcessor[] worker,
-            Queue<Status> queue) {
-
-        boolean run = true;
-        while (run) {
-            System.out
-                    .println("Enter 'exit' to exit or 'status' to view the current status: ");
-            String in = "";
-            try {
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(System.in));
-                in = reader.readLine();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            if (in.equals("exit")) {
-                run = false;
-
-            } else if (in.equals("status")) {
-                // print status
-                System.out.println("STATE OF THE CRAWLER: ");
-                System.out.println("Number of status-objects in queue: "
-                        + queue.size());
-                // System.out.println("Status of the Streamlistener: ");
-                int c = 0;
-                for (int i = 0; i < workerThreads.length; i++) {
-                    if (workerThreads[i].isAlive()) {
-                        c++;
-                    }
-                }
-                System.out.println("Number of running workers: " + c + "/"
-                        + THREADNUM);
-            }
-        }
-
-        System.out.println("Terminating crawler ...");
-
-        sl.exit();
-
-        // worker stop if queue is empty
-        for (int i = 0; i < worker.length; i++) {
-            worker[i].run = false;
-        }
-
-        // wait till all workers ended
-        for (int i = 0; i < workerThreads.length; i++) {
-            try {
-                workerThreads[i].join();
-            } catch (InterruptedException e) {
-                logger.warning(e.getMessage());
-                // TODO
-                e.printStackTrace();
-            }
-        }
-
-        System.out.println("Crawler terminated");
-        logger.info("Program terminated by user");
-        System.exit(0);
-
+    private static void userInput() {
+    	BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+    	String command = "";   	
+		while (!command.equals("exit")) {
+			System.out.print("crawler> ");
+			try {
+				command = console.readLine();
+			} catch (IOException e) {
+				System.out.println(" Error, problem with reading user input.");
+			}
+			switch (command) {
+				case "status":
+					System.out.println(cntrl.toString());
+					break;
+				case "":
+					break;
+				case "exit":
+					break;
+				default:
+					System.out.println(" Error, could not find command '" + command + "'.");;
+			}
+		}
+		cntrl.shutdown();
+		System.exit(0);
     }
-
-    private static Logger getLogger() throws SecurityException, IOException {
-
-        Logger l = Logger.getLogger("logger");
-        FileHandler fh;
-        fh = new FileHandler("LogFile.log", true);
-        SimpleFormatter formatter = new SimpleFormatter();
-        fh.setFormatter(formatter);
-        l.addHandler(fh);
-        // true: print output on console and into file
-        // false: only store output in logFile
-        l.setUseParentHandlers(false);
-
-        return l;
-    }
-
 }
