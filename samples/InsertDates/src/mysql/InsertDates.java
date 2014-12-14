1a778ca4c41ace23326b14159a3268af4c052c9c
holger-e <cross-freak@freenet.de>
2014-12-14 13:08:11 +0100

    Entwurfsdokumente aktualisiert

--- samples/InsertDates/src/mysql/InsertDates.java
+++ samples/InsertDates/src/mysql/InsertDates.java
@@ -14,13 +14,11 @@ public class InsertDates {
      * insert dates into the database
      * 
      * @param args
-     *            1. Argument: The run-time in seconds that the crawler should
-     *            run (0 for infinity); 2.Argument: The hostname of the database
-     *            to use; 3.Argument: The port of the database; 4.Argument: The
-     *            name of the database to use; 5.Argument: The user of the
-     *            database to connect with; 6. Argument: The password for the
-     *            root user of the database twitter; no more arguments are
-     *            required
+     *            1..Argument: The hostname of the database to use; 2.Argument:
+     *            The port of the database; 3.Argument: The name of the database
+     *            to use; 4.Argument: The user of the database to connect with;
+     *            5. Argument: The password for the root user of the database
+     *            twitter; no more arguments are required
      * */
     public static void main(String[] args) throws ParseException {
 
@@ -38,10 +36,9 @@ public class InsertDates {
         // // TODO Auto-generated catch block
         // e.printStackTrace();
         // }
-        if (args.length > 5 && args[0].matches("[0-9]+")
-                && args[2].matches("[0-9]+") && args[1].length() > 0
-                && args[3].length() > 0 && args[4].length() > 0
-                && args[5].length() > 0) {
+        if (args.length > 4 && args[1].matches("[0-9]+")
+                && args[0].length() > 0 && args[2].length() > 0
+                && args[3].length() > 0 && args[4].length() > 0) {
 
             try {
                 DBcrawler t = new DBcrawler(new AccessData(args[1], args[2],
