89d782fa6fdd10c6e3919253c636ffcff4fc2da4
unknown <uxdir@student.kit.edu>
2015-01-24 14:36:29 +0100

    databaseOptions Kategorie hinzufügen implementiert

--- code/GUI/src/gui/databaseOptions/DatabaseOptController.java
+++ code/GUI/src/gui/databaseOptions/DatabaseOptController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import mysql.result.Account;
 import mysql.result.Category;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
@@ -13,6 +14,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
@@ -31,17 +33,30 @@ public class DatabaseOptController extends InputElement implements Initializable
 	private MenuItem addLoc;
 	@FXML
 	private MenuItem addAcount;
-	@FXML
-	private Button zurueck;
 	@FXML 
-	private Button fertig;
+	private Button bFertig;
+	@FXML
+	private Button bEntf;
+	@FXML
+	private Button bZurueck;
 	@FXML
 	private TreeView<Category> trvCat;
 	@FXML
 	private TextField txtCatSearch;
 	@FXML
 	private ListView<Category> listCat;
-
+	@FXML
+	private TextField txtAccountSearch;
+	@FXML
+	private Button bWeiter;
+	@FXML
+	private ListView<Account> listAccount;
+	@FXML 
+	private Label lbInfo;
+	
+	private Account account;
+	private Stage dialogStage;
+	
 	@Override
 	public void update(UpdateType type) {
 		// TODO Auto-generated method stub
@@ -56,12 +71,22 @@ public class DatabaseOptController extends InputElement implements Initializable
 			//TODO addLoc
 			//TODO addAcount
 		}
+		//popUp select account
+		if(txtAccountSearch != null) {
+			txtAccountSearch.setOnKeyPressed(new MyEventHandler());
+			listAccount.setOnMouseClicked(new MyEventHandler());
+			bWeiter.setOnMouseClicked(new MyEventHandler());
+		}
 		//popUp add Category
 		if(txtCatSearch != null) {
 			System.out.println("eingabe");
 			txtCatSearch.setOnKeyPressed(new MyEventHandler());
 			trvCat.setOnMouseClicked(new MyEventHandler());
+			bEntf.setOnMouseClicked(new MyEventHandler());
+			bFertig.setOnMouseClicked(new MyEventHandler());
+			bZurueck.setOnMouseClicked(new MyEventHandler());
 		}
+		
 	}
 	/**
 	 * creates tree for treeView
@@ -83,6 +108,16 @@ public class DatabaseOptController extends InputElement implements Initializable
 		}
 	}
 	/**
+	 * Sets the current stage
+	 * @param stage current stage
+	 */
+	public void setCurrentStage(Stage stage) {
+		dialogStage = stage;
+	}
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	/**
 	 * Inner class that manages all ActionEvent activities
 	 * @author Matthias
 	 *
@@ -96,19 +131,24 @@ public class DatabaseOptController extends InputElement implements Initializable
 			if (event.getSource().equals(addCat)) {
 				// popUp for InputDialog
 				// Load the fxml file and create a new stage for the popup
-			    FXMLLoader loader = new FXMLLoader(DatabaseOptController.class.getResource("CatSelect.fxml"));
+			    FXMLLoader loader = new FXMLLoader(DatabaseOptController.class.getResource("AccountSelect.fxml"));
+			    
 			    AnchorPane page;
 			    
 				try {
-					
+					System.out.println(DatabaseOptController.this);
 					page = (AnchorPane) loader.load();
-					Stage dialogStage = new Stage();
-				    dialogStage.setTitle("Kategorie auswählen");
+			        dialogStage = new Stage();
+			        
+			        // give the controller in the newly created thread a reference to the current stage;
+			        ((DatabaseOptController)loader.getController()).setCurrentStage(dialogStage);
+				    
+			        dialogStage.setTitle("Account auswählen");
 				    dialogStage.initModality(Modality.WINDOW_MODAL);
 				    //dialogStage.initOwner(primaryStage);
 				    Scene scene = new Scene(page);
 				    dialogStage.setScene(scene);
-				    dialogStage.showAndWait();
+				    dialogStage.show();
 				} catch (IOException e1) {
 					
 					System.out.println("Unable to construct popUp");
@@ -128,14 +168,123 @@ public class DatabaseOptController extends InputElement implements Initializable
 		@Override
 		public void handle(Event event) {
 			System.out.println("kontroller:" + event.getSource());
+		
+	    //######################################################	
+		//#PopUp Select Account
+		//########################################################
+			if(event.getSource().equals(txtAccountSearch)) {
+				// update ListView of accounts
+				
+				String input = txtAccountSearch.getText();
+				listAccount.getItems().clear();
+				for(Account a : superController.getAccounts(input)) {
+					listAccount.getItems().add(a);
+				}
+			}
+			if(event.getSource().equals(bWeiter)) {
+				// save selected account, close select-account-popUp
+				
+				Account selectedAccount = listAccount.getSelectionModel().getSelectedItem();
+				if (selectedAccount == null) {
+					lbInfo.setText("Kein Account ausgewählt");
+					return;
+				}
+				System.out.println(selectedAccount.getName());
+				dialogStage.close();
+				
+				// popUp for InputDialog
+				// Load the fxml file and create a new stage for the popup
+			    FXMLLoader loader = new FXMLLoader(DatabaseOptController.class.getResource("CatSelect.fxml"));
+			    
+			    AnchorPane page;
+			    
+				try {
+					System.out.println(DatabaseOptController.this);
+					page = (AnchorPane) loader.load();
+			        dialogStage = new Stage();
+			        
+			        // give the controller in the newly created thread a reference to the current stage and the 
+			        // selected account
+			        ((DatabaseOptController)loader.getController()).setCurrentStage(dialogStage);
+			        ((DatabaseOptController)loader.getController()).setAccount(selectedAccount);
+			        
+			        dialogStage.setTitle("Kategorie auswählen");
+				    dialogStage.initModality(Modality.WINDOW_MODAL);
+				    //dialogStage.initOwner(primaryStage);
+				    Scene scene = new Scene(page);
+				    dialogStage.setScene(scene);
+				    dialogStage.show();
+				} catch (IOException e1) {
+					
+					System.out.println("Unable to construct popUp");
+					e1.printStackTrace();
+				}    
+			}
+			if(event.getSource().equals(bZurueck)) {
+				//close select-category-popUp and open select-account-popUp again
+				dialogStage.close();
+				
+				FXMLLoader loader = new FXMLLoader(DatabaseOptController.class.getResource("AccountSelect.fxml"));
+			    
+			    AnchorPane page;
+			    
+				try {
+					System.out.println(DatabaseOptController.this);
+					page = (AnchorPane) loader.load();
+			        dialogStage = new Stage();
+			        
+			        // give the controller in the newly created thread a reference to the current stage
+			        ((DatabaseOptController)loader.getController()).setCurrentStage(dialogStage);
+			        
+			        dialogStage.setTitle("Account auswählen");
+				    dialogStage.initModality(Modality.WINDOW_MODAL);
+				    //dialogStage.initOwner(primaryStage);
+				    Scene scene = new Scene(page);
+				    dialogStage.setScene(scene);
+				    dialogStage.show();
+				} catch (IOException e1) {
+					
+					System.out.println("Unable to construct popUp");
+					e1.printStackTrace();
+				}    
+				
+			}
+			
+		//####################################################	
+		//#PopUp select category
+		//####################################################
 			if(event.getSource().equals(txtCatSearch)) {
+				// update TreeView of categories
 				String input = txtCatSearch.getText();
 				System.out.println(input);
 				updateCategory(superController.getCategoryRoot(input));
 			}
 			if(event.getSource().equals(trvCat)) {
+				// add a category to list of selected categories
 				Category cat = trvCat.getSelectionModel().getSelectedItem().getValue();
-				listCat.getItems().add(cat);
+				if(!listCat.getItems().contains(cat)) {
+					//  add a category only once
+					listCat.getItems().add(cat);
+				}
+				
+			}
+			if(event.getSource().equals(bEntf)) {
+				// remove a category from list of selected categories
+				int index =listCat.getSelectionModel().getSelectedIndex();
+				System.out.println(index);
+				if (index >= 0) {
+					listCat.getItems().remove(index);
+					listCat.setItems(listCat.getItems());
+				}
+			}
+			if(event.getSource().equals(bFertig)) {
+				// add selected Categories to database
+				System.out.println("bFertig xxxx");
+				for(Category cat : listCat.getItems()) {
+					superController.setCategory(account.getId(), cat.getId());
+				}	
+				//System.out.println(DatabaseOptController.this);
+				dialogStage.close();
 			}
 			
 		}
