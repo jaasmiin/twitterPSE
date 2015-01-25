91ef7ca027fb1dad72de084ca945e3a2464cfe04
m--i <m--i@awiszus.info>
2015-01-25 17:43:46 +0100

    TestDialog als JFrame hinzugefügt.

--- code/GUI/src/gui/standardMap/StandardMapController.java
+++ code/GUI/src/gui/standardMap/StandardMapController.java
@@ -2,20 +2,17 @@ package gui.standardMap;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
 
 import processing.core.PApplet;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.embed.swing.SwingNode;
 import unfolding.MyUnfoldingMap;
 import mysql.result.TweetsAndRetweets;
@@ -39,13 +36,32 @@ public class StandardMapController extends OutputElement implements Initializabl
     
     private void addMapToPane() {
         mapSwingNode = new SwingNode();
+        mapSwingNode.minHeight(300);
         mapSwingNode.maxHeight(300);
         mapSwingNode.minWidth(300);
         map = new MyUnfoldingMap();
         
-        final JFrame mapJFrame = new MyFrame();
-    
-        mapSwingNode.setContent(mapJFrame.getRootPane());
+        JFrame jf = new JFrame();
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setBounds(100, 100, 450, 300);
+		JPanel contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		jf.setContentPane(contentPane);
+		PApplet map = new MyUnfoldingMap();
+		jf.getContentPane().add(map);
+		map.init();
+		
+//        jf.setLayout(new FlowLayout());
+//        PApplet map = new MyUnfoldingMap();
+//        jf.add(map);
+//        jf.setPreferredSize(new Dimension(200, 200));
+//        System.out.println("abc");
+//        jf.getRootPane().setSize(200, 200);
+//        map.init();
+//        mapJFrame.getRootPane().setSize(300, 300);
+        
+        mapSwingNode.setContent(jf.getRootPane());
         
         
     }
--- code/GUI/src/unfolding/MyUnfoldingMap.java
+++ code/GUI/src/unfolding/MyUnfoldingMap.java
@@ -3,7 +3,6 @@ package unfolding;
  * 
  */
 
-import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
