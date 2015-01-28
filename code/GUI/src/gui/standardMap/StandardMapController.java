bcd093b9b461a0bcce9c49130befe5ade4b2f73c
m--i <m--i@awiszus.info>
2015-01-29 00:54:41 +0100

    Karte läd ohne Fehlermeldungen.

--- code/GUI/src/gui/standardMap/StandardMapController.java
+++ code/GUI/src/gui/standardMap/StandardMapController.java
@@ -1,38 +1,21 @@
 package gui.standardMap;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Window;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javax.swing.JFrame;
-import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.border.EmptyBorder;
 
-import processing.core.PApplet;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.embed.swing.SwingNode;
 import unfolding.MyUnfoldingMap;
 import mysql.result.TweetsAndRetweets;
 import gui.OutputElement;
-import gui.RunnableParameter;
 
 public class StandardMapController extends OutputElement implements Initializable {
     
     @FXML
     private SwingNode mapSwingNode;
-    @FXML
-    private StackPane pane;
     
     TweetsAndRetweets uneditedData;
     MyUnfoldingMap map;  
@@ -45,40 +28,31 @@ public class StandardMapController extends OutputElement implements Initializabl
     	
     	
     	
-        createThread(mapSwingNode);
+        insertMap();
         
     } 
     
-    private void createThread(final SwingNode s) {
-        s.resize(900, 600);
-        pane.setMinSize(400, 400);
+    private void insertMap() {
         map = new MyUnfoldingMap();
-        map.setSize(900, 600);
-        map.displayHeight = 600;
-        map.displayWidth = 900;
-        
-        map.init();
-       
-        System.out.println();
-        JLayeredPane contentPane = new JLayeredPane();
-
+		map.init();
+		
+		JPanel contentPane = new JPanel();
         contentPane.setSize(900, 600);
-                  
-                       
-        try {
-             Thread.sleep(1500);
-        } catch (InterruptedException e) {
-             // TODO Auto-generated catch block
-             e.printStackTrace();
-        }
-                        
-        contentPane.add(map);
-        contentPane.moveToFront(map);
-        s.setContent(contentPane);
-
-        map.setVisible(true);
-        map.loop();        
-        map.redraw();
+    	mapSwingNode.resize(contentPane.getWidth(), contentPane.getHeight());                
+        mapSwingNode.setContent(contentPane);        
+		mapSwingNode.getContent().add(map);
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		map.redraw();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		mapSwingNode.getContent().repaint();
 }
     
         
