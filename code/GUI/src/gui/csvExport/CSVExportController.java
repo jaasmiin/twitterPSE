eb38eda141a3035e092618fb07f46d296aacd65e
m--i <m--i@awiszus.info>
2015-02-01 16:06:33 +0100

    @holger-e s CSV-Exportmethode in GUI integriert.
    Und: Mapserst nach erfolgreichem Laden der GUI anzeigen.
    => GUI ist im Vordergund geöffnet.

---
+++ code/GUI/src/gui/csvExport/CSVExportController.java
@@ -0,0 +1,158 @@
+package gui.csvExport;
+
+import gui.InputElement;
+import gui.Labels;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+import mysql.result.Account;
+import mysql.result.Location;
+import mysql.result.Retweets;
+
+/**
+ * class provides method to export data into a csv file
+ * 
+ * @author Holger Ebhart and Maximilian Awiszus
+ * 
+ */
+public class CSVExportController extends InputElement implements Initializable {
+	@FXML
+	private MenuItem mnFile;
+	@FXML
+	private MenuItem mniExport;
+    /**
+     * exports the current data into a csv file
+     * 
+     * @param accounts
+     *            a list of the loaded accounts as List<Account>
+     * @param locations
+     *            a list of the available locations as List<Location>
+     * @param stage
+     *            the parent stage to show the save-file-dialog as final Stage
+     */
+    public static void exportAsCSV(List<Account> accounts,
+            List<Location> locations, final Stage stage) {
+        String path = getFilePath(stage);
+        if (path == null)
+            return;
+        File file = new File(path);
+        String[][] string = buildFile(accounts, locations);
+        writeFile(file, string);
+    }
+
+    private static String getFilePath(final Stage stage) {
+
+        // only show .csv files
+        ExtensionFilter ef = new ExtensionFilter("csv", "*.csv");
+
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().add(ef);
+        fc.setTitle("Speichern unter...");
+
+        String path = fc.showSaveDialog(stage).getAbsolutePath();
+        if (path != null) {
+            if (!path.endsWith(".csv")) {
+                path = path + ".csv";
+            }
+        }
+
+        return path;
+    }
+
+    private static void writeFile(File file, String[][] string) {
+
+        Writer w;
+        try {
+            w = new FileWriter(file);
+        } catch (IOException e1) {
+            return;
+        }
+        BufferedWriter writer = new BufferedWriter(w);
+        for (String[] x : string) {
+            String line = x[0];
+            for (int i = 1; i < x.length; i++) {
+                line += "," + x[i];
+            }
+
+            try {
+                writer.append(line);
+                writer.newLine();
+            } catch (IOException e) {
+            }
+        }
+        try {
+            writer.close();
+        } catch (IOException e) {
+        }
+
+    }
+
+    private static String[][] buildFile(List<Account> accounts,
+            List<Location> locations) {
+
+        HashMap<String, Integer> h = new HashMap<String, Integer>();
+
+        String[][] file = new String[accounts.size() + 1][locations.size() + 3];
+        file[0][0] = "Accountname";
+        file[0][1] = "Country";
+        file[0][2] = "Follower";
+        int i = 3;
+        for (Location l : locations) {
+            file[0][i] = l.toString();
+            h.put(l.getLocationCode(), i);
+            i++;
+        }
+
+        int j = 1;
+        for (Account a : accounts) {
+            file[j][0] = a.getName();
+            file[j][1] = a.getLocationCode();
+            file[j][2] = String.valueOf(a.getFollower());
+
+            for (Retweets r : a.getRetweets()) {
+                file[j][h.get(r.getLocationCode())] = String.valueOf(r
+                        .getCounter());
+            }
+
+            j++;
+        }
+
+        return file;
+    }
+
+	@Override
+	public void update(UpdateType type) {}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		super.initialize(location, resources);
+		superController.subscribe(this);
+		mnFile.setText(Labels.FILE);
+		mniExport.setText(Labels.EXPORT);
+		mniExport.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				superController.setInfo(Labels.EXPORTING);
+				exportAsCSV(superController.getDataByAccount(),
+						superController.getLocations(), superController.getStage());
+				superController.setInfo(Labels.EXPORTED, Labels.EXPORTING);
+			}
+		});
+	}
+}
