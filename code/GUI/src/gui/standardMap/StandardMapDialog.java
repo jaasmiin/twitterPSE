1d016af21208621434567d16dfa0ad852cf2c427
li-gri <lidia.grigoriev@gmail.com>
2015-02-02 22:57:45 +0100

    TimeSlider in StandardMap überführt (Version1)

--- code/GUI/src/gui/standardMap/StandardMapDialog.java
+++ code/GUI/src/gui/standardMap/StandardMapDialog.java
@@ -4,6 +4,8 @@ package gui.standardMap;
 import gui.GUIController;
 import gui.GUIElement.UpdateType;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -25,26 +27,58 @@ public class StandardMapDialog extends JDialog {
 		setSize(600, 400);
 		setTitle("Map");
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-		map = map.getSingleton(superController);
+		map = MyUnfoldingMap.getInstance(superController);
+		System.out.println();
 		add(map);
 		map.init();
 	}
 
-	public void update(UpdateType type) {
+	   /**
+     * updates the map
+     * @param type type of the update event
+     * @param start start date of range of the query
+     * @param end end date of range of the query
+     */
+    public void update(UpdateType type, LocalDate start, LocalDate end) {
 		switch (type) {
 		case CLOSE:
 			if (map != null) {
 				map.exit();
 			}
 			break;
-		case TWEET:
-			uneditedData = superController.getDataByLocation();
+		case TWEET_BY_DATE:
+		    
+		 // aggregate relevant data and check if dates are valid
+            System.out.println();
+            if (start == null) {
+                start = LocalDate.MAX;
+            }
+            if (end == null) {
+                end = LocalDate.MIN;
+            }
+		    
+			uneditedData = superController.getDataByLocationAndDate();
 			HashMap<String, Integer> forCalc = new HashMap<String, Integer>();
 			for (mysql.result.Retweets r : uneditedData.getRetweets()) {
-				int counter = r.getCounter();
-				String id = r.getLocationCode();
-				forCalc.put(id, counter);
-				
+			    Date startTest = new Date(start.getYear(), start.getMonthValue(), start.getDayOfMonth());
+                Date endTest = new Date(end.getYear(), end.getMonthValue(), end.getDayOfMonth());
+                
+                //Check if Tweet/Retweet-Odjacts Date is in the needed interval
+                if((r.getDate().after(startTest) && r.getDate().before(endTest)) ||
+                        r.getDate().equals(endTest) || r.getDate().equals(startTest)) {
+                    
+                    //Check if counter for location is already in the hashMap
+                    if(forCalc.containsKey(r.getLocationCode())){
+                        int count = forCalc.get(r.getLocationCode());
+                        count += r.getCounter();
+                        forCalc.put(r.getLocationCode(), count);
+                    }
+                    else {
+                        int counter = r.getCounter();
+                        String id = r.getLocationCode();
+                        forCalc.put(id, counter);
+                    }
+                }
 			}
 			Set<String> keySet = forCalc.keySet();
 			for (String key : keySet) {
