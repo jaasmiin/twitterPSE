4f43deae08b2d9fb316096b09c9d9882aa03b5f1
Philipp <ufedz@student.kit.edu>
2015-02-03 08:31:20 +0100

    Package-Info fuer Tabelle

---
+++ code/GUI/src/gui/table/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * A table where the retweet information given by
+ * a combination of accounts, categories and locations
+ * is displayed by accounts and those accounts' retweets
+ * per location.
+ * 
+ * @author Philipp
+ *
+ */
+package gui.table;
